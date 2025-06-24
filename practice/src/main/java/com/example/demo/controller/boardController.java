57b9dbfbd72d84be32a0daaaa9c5c2d15531e8e7
yeonggyo <dldudry2222@gmail.com>
2025-06-24 12:38:36 +0900

    boardController 기본

--- practice/src/main/java/com/example/demo/controller/boardController.java
+++ practice/src/main/java/com/example/demo/controller/boardController.java
@@ -1,5 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class boardController {
 
+	@GetMapping("/")
+	public String list() {
+		return "board/list";
+	}
+	@GetMapping("/write")
+	public String write() {
+		return "board/write";
+	}
+	@GetMapping("/update")
+	public String update() {
+		return "board/update";
+	}
 }
